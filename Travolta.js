21942eeb580846a209a5c72180d6405dd93be2b4
stefaneyd <stefan.eydal@gmail.com>
2024-01-11 13:46:02 +0000

    Create Travolta.js

---
+++ Travolta.js
@@ -0,0 +1,10 @@
+javascript:(function(){
+  var travolta = document.createElement("img");
+  travolta.src = "http://i.imgur.com/vYCRrc0.gif";
+  document.body.appendChild(travolta);
+  travolta.style.position = "fixed";
+  travolta.style.top = "calc(100% - 476px)";
+  travolta.style.left = "calc(50% - 245px)";
+  travolta.style.zIndex = "200000000";
+  travolta.style.pointerEvents="none";
+})();
